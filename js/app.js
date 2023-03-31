1508aa4c23e8afb0722f9d712bd6c8b9f434b04b
lilhalzy <lilhalzy11@gmail.com>
2023-03-31 22:16:11 +0800

    feat: added game logic
    win,lose, and tie

--- js/app.js
+++ js/app.js
@@ -9,6 +9,36 @@ const App = { //This is NameSpace
     state: {
         turns: [],
     },
+    gameTracker(turns) {
+        const p1Turns = turns.filter(turn => turn.playerId === 1).map(move => +move.boxId)
+        const p2Turns = turns.filter(turn => turn.playerId === 2).map(move => +move.boxId)
+
+        const victoryTracker = [
+            [1,2,3], 
+            [1,5,9],
+            [1,4,7],
+            [2,5,8],
+            [3,5,7],
+            [3,6,9],
+            [4,5,6],
+            [7,8,9],
+        ]
+        
+        let winner = null
+        
+        victoryTracker.forEach(tracker => {
+            const p1Wins = tracker.every(value => p1Turns.includes(value))
+            const p2Wins = tracker.every(value => p2Turns.includes(value))
+            
+            if(p1Wins) winner = 'X'
+            if(p2Wins) winner = 'O'
+        })
+        
+        return {
+            result: turns.length === 9 || winner != null ? 'complete' : 'in-progress',
+            winner,
+        }
+    },
     init() { //init() - apply the registeredEL (more organized rather than declaring the even listener inside the init method)
         App.registerEventListeners()
     },
@@ -29,26 +59,37 @@ const App = { //This is NameSpace
                     const existingTurn = App.state.turns.find(turn => turn.boxId === boxId)
                     return existingTurn !== undefined
                 }
-
+                
                 if(hasTurn(+box.id)) {
                     return
                 }
 
                 const lastTurn = App.state.turns.at(-1) // at(-1) will grab the last element of that array
-
+                
                 const icon = document.createElement('i')
-
+                
                 const getOppositePlayer = playerId => playerId === 1 ? 2 : 1
                 const currentPlayer = App.state.turns.length === 0 ? 1 : getOppositePlayer(lastTurn.playerId)
-
+                
                 currentPlayer === 1 ? icon.classList.add('fa-solid', 'fa-x', 'violet') : icon.classList.add('fa-solid', 'fa-o', 'lavender')
-
+                
                 App.state.turns.push({  // Tracking state over the lifecycle app
                     boxId: +box.id,
                     playerId: currentPlayer,
                 })
-
+                
                 box.replaceChildren(icon)
+
+                // check if there's a winner || tie
+                const game = App.gameTracker(App.state.turns)
+
+                if (game.result === 'complete') {
+                    if (game.winner) {
+                        alert (`Player ${game.winner} wins!`)
+                    } else {
+                        alert (`Tie!`)
+                    }
+                }
             })
         })
     },
