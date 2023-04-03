7d3955b07b3d67c3cf5a44ae84803a722e0cc596
lilhalzy <lilhalzy11@gmail.com>
2023-04-03 09:53:04 +0800

    refactor: improved codebase
    renamed the namespace and use document selector for easy debugging in near future

--- js/view.js
+++ js/view.js
@@ -1,22 +1,24 @@
 export default class View {
 
     $ = {}
+    $$ = {}
 
     constructor() {
-        this.$.action = document.querySelector('[data-id="actions"]')
-        this.$.actionBtn = document.querySelector('[data-id="action-button"]')
-        this.$.actionItems = document.querySelector('[data-id="action-pop"]')
-        this.$.resetBtn = document.querySelector('[data-id="reset-btn"]')
-        this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]')
-        this.$.boxes = document.querySelectorAll('[data-id="box"]')
-        this.$.modal = document.querySelector('[data-id="modal"]')
-        this.$.modalTxt = document.querySelector('[data-id="modal-txt"]')
-        this.$.modalBtn = document.querySelector('[data-id="modal-btn"]')
-        this.$.turn = document.querySelector('[data-id="turn"]')
+        this.$.action = this.#qs('[data-id="actions"]')
+        this.$.actionBtn = this.#qs('[data-id="action-button"]')
+        this.$.actionItems = this.#qs('[data-id="action-pop"]')
+        this.$.resetBtn = this.#qs('[data-id="reset-btn"]')
+        this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]')
+        this.$.modal = this.#qs('[data-id="modal"]')
+        this.$.modalTxt = this.#qs('[data-id="modal-txt"]')
+        this.$.modalBtn = this.#qs('[data-id="modal-btn"]')
+        this.$.turn = this.#qs('[data-id="turn"]')
+
+        this.$$.boxes = this.#qsAll('[data-id="box"]')
 
         // UI event listeners
         this.$.actionBtn.addEventListener('click', e => {
-            this.toggleItems()
+            this.#toggleItems()
         })
         this.$.resetBtn.addEventListener('click', e => {
 
@@ -36,8 +38,31 @@ export default class View {
     }
     
     bindPlayerTurnEvent(handler) {
-        this.$.boxes.forEach(box => {
+        this.$$.boxes.forEach(box => {
             box.addEventListener('click', handler)
         })
+    }
+
+    /**
+     * DOM helper methods
+     */
+
+    // # - make it private || not public
+    #qs(selector, parent) {
+        const el = parent ? parent.querySelector(selector) : document.querySelector(selector)
+        
+        // abort the app continuation if there's no element selected
+        if (!el) throw new Error('Could not find the elements')
+
+        return el
+    }
+
+    #qsAll(selector) {
+        const elList = document.querySelectorAll(selector)
+
+        // abort the app continuation if there's no element selected
+        if(!elList) throw new Error('Could not find elements')
+
+        return elList
     }
 }
